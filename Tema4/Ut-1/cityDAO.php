58acf25beb25a791a94e3273a672881e3975205b
Jesus Sosa Morales <jesussosamorales@gmail.com>
2022-12-15 11:56:19 +0000

    Subiendo app php y .gitignore

---
+++ Tema4/Ut-1/cityDAO.php
@@ -0,0 +1,41 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+defined('__CONFIG__') || require_once ('config.php');
+
+function abrirConex(){
+    $host = __HOST;
+    $db = __DB;
+    $user = __USER;
+    $pass = __PASS;
+    return pg_connect("host=$host dbname=$db user=$user password=$pass");
+}
+
+
+
+
+
+
+
+
+function selectVisited(){
+    $conexion = abrirConex();
+    $ciudades = pg_exec($conexion, "SELECT * FROM places WHERE visited=true");
+
+    pg_close($conexion);
+    return $ciudades;
+
+}
+
+function selectNoVisited(){
+    $conexion = abrirConex();
+    $ciudades = pg_exec($conexion, "SELECT * FROM places WHERE visited=false");
+    pg_close($conexion);
+    return $ciudades;
+}
+
+
+
+
+?>
---
+++ Tema4/Ut-1/index.php
@@ -0,0 +1,48 @@
+<?php
+require_once("cityDAO.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>My Travel Bucket List</h1>
+<h2>Places I'd Like to Visit</h2>
+<ul>
+    <?php
+        $novisitadas = selectNoVisited();
+ 
+    while($row=pg_fetch_row($novisitadas)){
+        echo "<li> $row[1] </li>";
+
+    }
+
+   
+    ?>
+ 
+</ul>
+
+<h2>Places I've Already Been To</h2>
+
+<ul>
+  
+<?php
+        $visitadas = selectVisited();
+ 
+    while($row=pg_fetch_row($visitadas)){
+        echo "<li> $row[1] </li>";
+
+    }
+
+   
+    ?>
+
+</ul>
+
+    
+</body>
+</html>
